46eed07466cd521801486a60825bb3c84f9e113f
Shubham <shubhamdevu023@gmail.com>
2026-01-23 01:17:19 +0530

    Added prisma and connected to postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
