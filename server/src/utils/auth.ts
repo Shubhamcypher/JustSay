2b96a5c2b32020e602d7a20942188657cc58e734
Shubham <shubhamdevu023@gmail.com>
2026-02-12 19:55:39 +0530

    JWT and passwoed hashing

---
+++ server/src/utils/auth.ts
@@ -0,0 +1,31 @@
+import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
+import "dotenv/config";
+
+const SALT_ROUNDS = 10;
+
+export async function hashPassword(password: string) {
+  return bcrypt.hash(password, SALT_ROUNDS);
+}
+
+export async function comparePassword(
+  password: string,
+  hashedPassword: string
+) {
+  return bcrypt.compare(password, hashedPassword);
+}
+
+export function generateAccessToken(payload: object) {
+  const secret = process.env.JWT_ACCESS_SECRET;
+  const expires = process.env.JWT_ACCESS_EXPIRES_IN || "15m";
+
+  if (!secret) {
+    throw new Error("JWT_ACCESS_SECRET is not defined");
+  }
+
+  const options: SignOptions = {
+    expiresIn: expires as SignOptions["expiresIn"],
+  };
+
+  return jwt.sign(payload, secret, options);
+}
