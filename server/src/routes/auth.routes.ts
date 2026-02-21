b19170c1f6c4ce896c6775dd4001370c7f3299de
Shubham <shubhamdevu023@gmail.com>
2026-02-21 22:12:20 +0530

    added a test route

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { register, login, refresh, logout } from "../controller/auth.controller";
+import { authenticate, AuthRequest } from "../middleware/auth.middleware";
 
 const router = Router();
 
@@ -7,5 +8,12 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/refresh", refresh);
 router.post("/logout", logout);
+
+router.get("/me", authenticate, async (req: AuthRequest, res) => {
+    res.json({
+      message: "You are authenticated",
+      userId: req.user?.userId,
+    });
+  });
 
 export default router;
