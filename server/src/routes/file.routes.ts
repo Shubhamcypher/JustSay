2914d1db994bf8659153270ac244b561d185466c
Shubham <shubhamdevu023@gmail.com>
2026-03-14 21:19:02 +0530

    adding file routes

---
+++ server/src/routes/file.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { authenticate } from "../middleware/auth.middleware";
+import { createFile, deleteFile, getFiles, updateFile } from "../controller/file.controller";
+
+const router = Router();
+
+router.post("/", authenticate, createFile);
+router.get("/:projectId", authenticate, getFiles);
+router.put("/:id", authenticate, updateFile);
+router.delete("/:id", authenticate, deleteFile);
+
+export default router;
