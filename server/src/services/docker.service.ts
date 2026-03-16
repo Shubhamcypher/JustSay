ad8ddda364ad57598ca7baa9781e899b9fe8cfc6
Shubham <shubhamdevu023@gmail.com>
2026-03-16 22:13:46 +0530

    build project image function

--- server/src/services/docker.service.ts
+++ server/src/services/docker.service.ts
@@ -1,5 +1,20 @@
 import Docker from "dockerode";
 
-const docker = new Docker();
+const docker = new Docker({socketPath: "/var/run/docker.sock"});
+
+export async function buildProjectImage(projectPath: string, projectId: string) {
+
+    const stream = await docker.buildImage(
+      {
+        context: projectPath,
+        src: ["."]
+      },
+      {
+        t: `justsay-project-${projectId}`
+      }
+    );
+  
+    return stream;
+  }
 
 export default docker;
