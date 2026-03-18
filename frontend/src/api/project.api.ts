be6615637867f75bbeb0fd721cd79cde07f2d411
Shubham <shubhamdevu023@gmail.com>
2026-03-18 22:43:31 +0530

    added project.api.ts

---
+++ frontend/src/api/project.api.ts
@@ -0,0 +1,15 @@
+import API from "./axios";
+
+export const getProjects = () => API.get("/projects");
+
+export const createProject = (data: { name: string; stack: string }) =>
+  API.post("/projects", data);
+
+export const startProject = (id: string) =>
+  API.post(`/projects/${id}/start`);
+
+export const stopProject = (id: string) =>
+  API.post(`/projects/${id}/stop`);
+
+export const getFiles = (projectId: string) =>
+  API.get(`/files/${projectId}`);
