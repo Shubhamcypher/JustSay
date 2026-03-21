01fa9cd9f244bb36331a327bcb972b560646dcab
Shubham <shubhamdevu023@gmail.com>
2026-03-21 22:59:21 +0530

    updated start project api

--- frontend/src/api/project.api.ts
+++ frontend/src/api/project.api.ts
@@ -5,8 +5,12 @@ export const getProjects = () => API.get("/projects");
 export const createProject = (data: { name: string; stack: string }) =>
   API.post("/projects", data);
 
-export const startProject = (id: string) =>
-  API.post(`/projects/${id}/start`);
+// export const startProject = (id: string) =>
+//   API.post(`/projects/${id}/start`);
+export const startProject = async (projectId: string) => {
+  const res = await API.post(`/projects/${projectId}/start`);
+  return res.data;
+};
 
 export const stopProject = (id: string) =>
   API.post(`/projects/${id}/stop`);
