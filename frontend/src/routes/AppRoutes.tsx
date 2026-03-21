710a6c160a72e6463a4837670bc8f36a0ffca78a
Shubham <shubhamdevu023@gmail.com>
2026-03-21 23:01:18 +0530

    added route for project preview

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Project from "../pages/Project";
+import ProjectPreview from "../pages/ProjectPreview";
 
 export default function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/project/:id" element={<Project />} />
+        <Route path="/project/:id" element={<ProjectPreview />} />
       </Routes>
     </BrowserRouter>
   );
