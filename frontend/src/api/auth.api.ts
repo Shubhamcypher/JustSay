7b7d1686e82f5af3607966f688a4be206f12c3f4
Shubham <shubhamdevu023@gmail.com>
2026-03-23 23:14:43 +0530

    added auth.api.ts in frontend

---
+++ frontend/src/api/auth.api.ts
@@ -0,0 +1,13 @@
+import API from "./axios";
+
+export const register = (data: { email: string; password: string }) =>
+  API.post("/auth/register", data);
+
+export const login = (data: { email: string; password: string }) =>
+  API.post("/auth/login", data);
+
+export const refresh = (refreshToken: string) =>
+  API.post("/auth/refresh", { refreshToken });
+
+export const logout = (refreshToken: string) =>
+  API.post("/auth/logout", { refreshToken });
