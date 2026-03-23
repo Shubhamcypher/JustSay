b6ddf545ca9488760c66ca9d8b97d3bfd173d3a7
Shubham <shubhamdevu023@gmail.com>
2026-03-23 23:18:24 +0530

    added Login.tsx

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { login } from "../api/auth.api";
+import { setTokens } from "../utils/auth";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await login({ email, password });
+
+      setTokens(res.data.accessToken, res.data.refreshToken);
+
+      navigate("/");
+
+    } catch (err: any) {
+      alert(err.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
