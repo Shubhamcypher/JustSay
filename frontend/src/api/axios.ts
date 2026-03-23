a7b1583dbf3a331a8aaf4fb0325f4198e8623b46
Shubham <shubhamdevu023@gmail.com>
2026-03-23 23:29:05 +0530

    removed hardcoded token and now fetching from localstorage

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -6,7 +6,7 @@ const API = axios.create({
 
 // attach token automatically
 API.interceptors.request.use((config) => {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YzM1MmYyOS0zZTQxLTRkNTYtOWM1Zi01MzhmYTJmZDE3NTgiLCJpYXQiOjE3NzM4NTU4NTYsImV4cCI6MTc3Mzg1Njc1Nn0.ovCHiMPavMfLwtxyiMVAPsTBD_eiAMlqB1cAEKb_7mg";
+    const token = localStorage.getItem("accessToken");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
