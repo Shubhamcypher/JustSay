327219b54b5541efda42b0390daf7d0b2c5dcfd7
Shubham <shubhamdevu023@gmail.com>
2026-03-23 23:21:39 +0530

    added Register.tsx

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { register } from "../api/auth.api";
+import { setTokens } from "../utils/auth";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    try {
+      const res = await register({ email, password });
+
+      setTokens(res.data.accessToken, res.data.refreshToken);
+
+      navigate("/");
+
+    } catch (err: any) {
+      alert(err.response?.data?.message || "Register failed");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+}
