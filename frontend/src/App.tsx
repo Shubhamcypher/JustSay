db15a36b1061d9a47d187303086f410d97ec3355
Shubham <shubhamdevu023@gmail.com>
2026-03-24 14:25:13 +0530

    added tailwind!!!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css'
+import './index.css'
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
 
-  return <AppRoutes />;
+  // return <AppRoutes />;
+  return(
+    <div className='text-red-500'>
+      Tailwind is workingggggggg
+    </div>
+  )
+
 }
 
 export default App
