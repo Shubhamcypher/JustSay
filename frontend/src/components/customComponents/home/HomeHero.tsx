7af2316d2b4a27a8315654a7d103abd181ad75b3
Shubham <shubhamdevu023@gmail.com>
2026-03-30 21:51:31 +0530

    fixed text size and padding and styling forl all screens accelerating towards responsiveness

--- frontend/src/components/customComponents/home/HomeHero.tsx
+++ frontend/src/components/customComponents/home/HomeHero.tsx
@@ -23,15 +23,15 @@ export default function HomeHero() {
     return (
         <div className="relative min-h-[60vh] md:h-[80vh] flex md:items-center justify-center px-4 md:px-0 pt-6 md:pt-0 items-center">
 
-            <div className="relative w-full md:w-[50vw] max-w-4xl text-center">
+            <div className="relative w-full md:w-[100vw] md:p-6 max-w-4xl text-center">
 
                 {/* Heading */}
-                <h1 className="text-3xl sm:text-4xl md:text-6xl leading-[1.3] md:leading-[1.4] font-bold bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
+                <h1 className="text-4xl md:text-6xl lg:text-5xl leading-[1.3] md:leading-[1.4] font-bold bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 bg-clip-text text-transparent">
                     Build anything with AI
                 </h1>
 
                 {/* Subtitle */}
-                <p className="text-white/60 text-sm sm:text-base md:text-xl mt-2 md:mt-3 px-2 md:px-0">
+                <p className="text-white/60 text-md md:text-2xl mt-3 px-2 md:px-0">
                     Describe your idea and generate a full website.
                 </p>
 
