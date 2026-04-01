00a65dcc2475d2b073efca702d136c9cc439c219
Shubham <shubhamdevu023@gmail.com>
2026-04-02 03:08:15 +0530

    fixed user route for /me

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,17 +1,22 @@
 import { Router } from "express";
 import { pool } from "../config/db";
 import { updateProfile } from "../controller/user.controller";
-import { authenticate, AuthRequest } from "../middleware/auth.middleware";
+import { authenticate } from "../middleware/auth.middleware";
 
 const router = Router();
 
 
-router.get("/me", authenticate, async (req: AuthRequest, res) => {
+router.get("/me", authenticate, async (req, res) => {
     try {
+        const userId = (req.user as any).userId;
+
+        
         const result = await pool.query(
-            "SELECT id, email, role, created_at FROM users WHERE id = $1",
-            [req.user?.userId]
+            "SELECT id, email, created_at FROM users WHERE id = $1",
+            [userId]
         );
+
+        
 
         if (result.rows.length === 0) {
             return res.status(404).json({ message: "User not found" });
@@ -22,6 +27,8 @@ router.get("/me", authenticate, async (req: AuthRequest, res) => {
             data: result.rows[0],
         });
     } catch (err) {
+        console.log(err);
+        
         res.status(500).json({ message: "Server error" });
     }
 });
