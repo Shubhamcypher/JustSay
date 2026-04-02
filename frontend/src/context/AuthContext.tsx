6bea896aedee23b12db0d2fadf567983c58681c8
Shubham <shubhamdevu023@gmail.com>
2026-04-02 23:50:38 +0530

    adding auth context

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,5 +1,8 @@
+
+
+
 import { createContext, useContext, useState, useEffect } from "react";
-import { login, register, refresh } from "@/api/auth.api";
+import { login, register, refresh, getMe } from "@/api/auth.api";
 import {
     setTokens,
     getAccessToken,
@@ -19,6 +22,7 @@ type AuthContextType = {
     loginUser: (data: { email: string; password: string }) => Promise<void>;
     registerUser: (data: { email: string; password: string }) => Promise<void>;
     logoutUser: () => void;
+    setUserFromToken: ()=>void;
 };
 
 const AuthContext = createContext<AuthContextType | null>(null);
@@ -28,40 +32,63 @@ export function AuthProvider({ children }: any) {
     const [loading, setLoading] = useState(true);
 
     // 🔁 restore session
+    // useEffect(() => {
+    //     const init = async () => {
+    //         const access = getAccessToken();
+    //         const refreshToken = getRefreshToken();
+
+    //         if (!access || !refreshToken) {
+    //             setLoading(false);
+    //             return;
+    //         }
+
+    //         try {
+    //             const res = await refresh(refreshToken);
+
+    //             const { accessToken, refreshToken: newRefresh } = res.data;
+
+    //             setTokens(accessToken, newRefresh);
+
+    //             const decoded = decodeToken(accessToken);
+
+    //             if (decoded) {
+    //                 setUser({
+    //                     id: decoded.id,
+    //                     email: decoded.email,
+    //                 });
+    //             }
+    //         } catch {
+    //             clearTokens();
+    //             setUser(null);
+    //         } finally {
+    //             setLoading(false);
+    //         }
+    //     };
+
+    //     init();
+    // }, []);
+
     useEffect(() => {
-        const init = async () => {
-            const access = getAccessToken();
-            const refreshToken = getRefreshToken();
+        const access = getAccessToken();
 
-            if (!access || !refreshToken) {
-                setLoading(false);
-                return;
-            }
+        if (!access) {
+            setLoading(false);
+            return;
+        }
 
-            try {
-                const res = await refresh(refreshToken);
+        const decoded = decodeToken(access);
 
-                const { accessToken, refreshToken: newRefresh } = res.data;
+        if (!decoded) {
+            clearTokens();
+            setUser(null);
+        } else {
+            setUser({
+                id: decoded.id,
+                email: decoded.email,
+            });
+        }
 
-                setTokens(accessToken, newRefresh);
-
-                const decoded = decodeToken(accessToken);
-
-                if (decoded) {
-                    setUser({
-                        id: decoded.id,
-                        email: decoded.email,
-                    });
-                }
-            } catch {
-                clearTokens();
-                setUser(null);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        init();
+        setLoading(false);
     }, []);
 
     const registerUser = async (data: {
@@ -86,6 +113,8 @@ export function AuthProvider({ children }: any) {
 
     // 🔐 login
     const loginUser = async (data: { email: string; password: string }) => {
+        console.log(data);
+        
         const res = await login(data);
 
         const { accessToken, refreshToken } = res.data;
@@ -94,11 +123,17 @@ export function AuthProvider({ children }: any) {
 
         const decoded = decodeToken(accessToken);
 
+        console.log(decodeToken);
+        
+
         if (decoded) {
             setUser({
                 id: decoded.id,
                 email: decoded.email,
             });
+
+            console.log(user);
+            
         }
     };
 
@@ -108,9 +143,19 @@ export function AuthProvider({ children }: any) {
         setUser(null);
     };
 
+    const setUserFromToken = async () => {
+        try {
+            const res = await getMe();
+            setUser(res.data.data);
+        } catch {
+            clearTokens();
+            setUser(null);
+        }
+    };
+
     return (
         <AuthContext.Provider
-            value={{ user, loading, loginUser, registerUser, logoutUser }}
+            value={{ user, loading, loginUser, registerUser, logoutUser, setUserFromToken }}
         >
             {children}
         </AuthContext.Provider>
