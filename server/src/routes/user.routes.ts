1aeca9bd30daf9a81ce6fba4bd1c56884edebc16
Shubham <shubhamdevu023@gmail.com>
2026-04-03 23:53:20 +0530

    removing unncessary logs

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -8,20 +8,14 @@ const router = Router();
 
 router.get("/me", authenticate, async (req, res) => {
     try {
-        
-        const userId = (req.user as { userId: string }).userId;
-        console.log(userId);
 
-        
+        const userId = (req.user as { userId: string }).userId;
+
+
         const result = await pool.query(
             "SELECT id, email,username, img, created_at FROM users WHERE id = $1",
             [userId]
         );
-
-        // console.log(result);
-        
-
-        
 
         if (result.rows.length === 0) {
             return res.status(404).json({ message: "User not found" });
@@ -33,7 +27,7 @@ router.get("/me", authenticate, async (req, res) => {
         });
     } catch (err) {
         console.log(err);
-        
+
         res.status(500).json({ message: "Server error" });
     }
 });
