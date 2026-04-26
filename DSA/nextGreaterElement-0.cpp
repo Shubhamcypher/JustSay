1ade45b5ee64d5612f5860ef974b1b0c0280d6b8
Shubham <shubhamdevu023@gmail.com>
2026-04-26 23:44:21 +0530

    nextGreaterElement-0.cpp

---
+++ DSA/nextGreaterElement-0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    vector<int> arr = {6,8,0,1,3};
+
+    vector<int>ans;
+    stack<int> s;
+
+    int n = arr.size();
+
+    for(int i =arr.size()-1; i>=0; i--){
+        
+    }
+
+    return 0;
+}
