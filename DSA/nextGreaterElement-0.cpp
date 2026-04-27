4639b6a0ae5c921df877097f04143463484453d3
Shubham <shubhamdevu023@gmail.com>
2026-04-27 23:50:10 +0530

    DSA--->nextGreater element

--- DSA/nextGreaterElement-0.cpp
+++ DSA/nextGreaterElement-0.cpp
@@ -4,16 +4,29 @@
 
 using namespace std;
 
-int main(){
-    vector<int> arr = {6,8,0,1,3};
-
-    vector<int>ans;
-    stack<int> s;
-
+int main()
+{
+    vector<int> arr = {6, 8, 0, 1, 3};
     int n = arr.size();
 
-    for(int i =arr.size()-1; i>=0; i--){
-        
+    vector<int> ans(n);
+    stack<int> s;
+
+
+    for (int i = arr.size() - 1; i >= 0; i--){
+        while (!s.empty() && s.top() <= arr[i])
+            s.pop();
+
+        if (s.empty())
+        ans[i] = -1;
+        else
+        ans[i] = s.top();
+
+        s.push(arr[i]);
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<ans[i]<<" ";
     }
 
     return 0;
