11d32d63154a2cf3c4c5c82495ad936701c36556
Shubham <shubhamdevu023@gmail.com>
2026-05-10 23:10:08 +0530

    adding follow up route

--- server/src/routes/generate.routes.ts
+++ server/src/routes/generate.routes.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { authenticate } from "../middleware/auth.middleware";
 import { generateProject } from "../controller/generate.controller";
 import { generateRaw } from "../controller/generateRawController";
+import { followUpProject } from "../controller/followup.controller";
 
 
 const router = express.Router();
 
 router.post("/generate", authenticate, generateProject);
 router.post("/generate/raw", authenticate, generateRaw);
+router.post("/followup", authenticate, followUpProject);
 
 export default router;
